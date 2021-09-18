c4cc4b96dd5bf01ed50c6fa44cf93ca6e2d6234f
juliswang <juliswang@tencent.com>
2021-09-18 10:06:30 +0800

    init base project, add ffmpeg.

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,41 @@
+//
+// Created by julis.wang on 2021/9/18.
+//
+#include <jni.h>
+#include <string>
+#include <unistd.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+
+JNIEXPORT jstring JNICALL
+Java_julis_wang_ffmpeglearn_MainActivity_test(JNIEnv *env, jobject  /* this */) {
+    char info[40000] = {0};
+    AVCodec *c_temp = av_codec_next(NULL);
+    while (c_temp != NULL) {
+        if (c_temp->decode != NULL) {
+            sprintf(info, "%sdecode:", info);
+        } else {
+            sprintf(info, "%sencode:", info);
+        }
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s(video):", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s(audio):", info);
+                break;
+            default:
+                sprintf(info, "%s(other):", info);
+                break;
+        }
+        sprintf(info, "%s[%s]\n", info, c_temp->name);
+        c_temp = c_temp->next;
+    }
+    return env->NewStringUTF(info);
+
+}
+}
---
+++ app/src/main/java/julis/wang/ffmpeglearn/MainActivity.java
@@ -0,0 +1,20 @@
+package julis.wang.ffmpeglearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.loadLibrary("native-lib");
+        TextView textView = findViewById(R.id.tv_test);
+        textView.setText(test());
+    }
+
+    public native String test();
+}
